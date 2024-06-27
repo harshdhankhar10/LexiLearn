58ed9565fd08d396b5c18c18078c8f2a361d60de
Harsh <dhankhar14804@gmail.com>
2024-06-27 15:48:46 +0530

    Navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,14 @@ const Navbar = ({ isMenuOpen, toggleMenu }) => {
               <Link to="/blog" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                 Blog
               </Link>
+              <Link to="/blog" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                Login
+              </Link>
+              <Link to="/blog" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                Sign up
+              </Link>
             </div>
+            
             <div className="flex items-center">
               <button
                 onClick={toggleMenu}
